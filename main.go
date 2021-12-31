e7a053560f025c8e9ec96d2f797e3f303fe8435d
thanapongsj <thanapongsj1996@gmail.com>
2022-01-01 00:24:23 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+	"sk-todos/todo"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&todo.Todo{})
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	todoHandler := todo.NewTodoHandler(db)
+	r.POST("/todos", todoHandler.NewTask)
+
+	r.Run()
+}
---
+++ todo/todo.go
@@ -0,0 +1,47 @@
+package todo
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"title" binding:"required"`
+}
+
+// TableName กำหนดชื่อ table ด้วยตัวเอง
+func (Todo) TableName() string {
+	return "todos"
+}
+
+type TodoHandler struct {
+	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	r := t.db.Create(&todo)
+	if err := r.Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": todo.Model.ID,
+	})
+}
